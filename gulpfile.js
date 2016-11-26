bb12632836181dcc81006a97095a4670b619915a
Anant Garg <infinitegarg@gmail.com>
2016-11-26 18:28:05 +0530

    setup browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function(){
   return gulp.src('app/scss/**/*.scss')
     .pipe(sass()) // Using gulp-sass
     .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.reload({
-      stream: true
-		}))
 });
 
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: 'app'
-    },
-  })
-})
-
-gulp.task('watch', ['browserSync', 'sass'], function(){
+gulp.task('watch', ['sass'], function(){
   gulp.watch('app/scss/**/*.scss', ['sass']);
 })
 
