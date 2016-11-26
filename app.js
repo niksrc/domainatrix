3cc31782dea576268ed41a31a0c1f801ed4dda57
Nikhil Srivastava <niksrc@gmail.com>
2016-11-26 18:00:07 +0530

    serve index

--- app.js
+++ app.js
@@ -9,12 +9,17 @@ const app = express();
  * Shouldn't be needed after assets served through nginx directly :)
  */
 app.use(logger('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(favicon(path.resolve(__dirname, '/public/favicon.ico')));
 
 // Serve app
 app.get('/', (req, res) => {
-	res.send('');
+	if (process.env.NODE_ENV === 'dev') {
+		res.send('./app/index.html');
+	} else {
+		res.send('./build/index.html');
+	}
 });
 
 // Exports FTW
